05d1a904fbffe8c1509cf4d5231b41ba32a5977c
Elijah Zappe <53388831+emzappe@users.noreply.github.com>
2026-02-25 11:46:58 -0700

    Update CT.ino

--- CT.ino
+++ CT.ino
@@ -3,35 +3,39 @@ int elay = 8;
 int turns = 10;
 int button = 6;
 int potStep = A1;
-int photo = 13;
 int potSample = analogRead(potStep);
+int numberOfSteps = 15;
 const uint8_t DirPin = 2;
 const uint8_t StepPin = 3;
 const uint8_t CSPin = 4;
 
 void setup() {
+  pinMode(potSample, INPUT);
   pinMode(button, INPUT);
   pinMode(Relay, OUTPUT);
   pinMode(elay, OUTPUT);
   pinMode(DirPin, OUTPUT);
   pinMode(StepPin, OUTPUT);
   Serial.begin(9600); 
-  int numberOfSteps = map(potSample, 0, 1023, 20, 300);
-  int potSample = analogRead(potStep);
   
-
+  
   
 }
 
 void loop() {
+  int potSample = analogRead(potStep);
+  int numberOfSteps = map(potSample, 0, 1023, 1, 100);
+  delay(1000);
+  int x = numberOfSteps;
   if (digitalRead(button) == HIGH) {
-  CT();
+    CT(x);
+   
   }
-
+  
 }
 
-void CT() {
-  int numCycles = photo; 
+void CT(int x) {
+  int numCycles = x; 
   for (int i = 0; i < numCycles; i++) {
     {
     digitalWrite(elay, LOW);
@@ -44,9 +48,10 @@ void CT() {
     digitalWrite(Relay, LOW);
     digitalWrite(elay, LOW);
   }
+  delay(500);
 
- {
-    digitalWrite(DirPin, LOW);
+  digitalWrite(DirPin, LOW);
+  for (int x = 0; x < 3200; x++) {
     digitalWrite(StepPin, HIGH);
     delayMicroseconds(200);
     digitalWrite(StepPin, LOW);
